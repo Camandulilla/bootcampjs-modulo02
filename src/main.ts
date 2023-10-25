62ec8175a9ca158569cf397a3bcf314cb2a13036
Jonathan <jonathanking.dev@gmail.com>
2023-10-25 10:49:52 +0200

    change with solution

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,5 @@ var bebidas = 18;
 var resultado;
 
 //Invitamos a la bebida nosotros, con lo cual debemos restar al total de la cuenta los 18€ de las bebidas
-resultado = (cuenta - bebidas) / 6;
+resultado = (cuenta - bebidas) / amigos;
 console.log("Cada comensal debe pagar " + resultado + "€");
