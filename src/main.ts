2de7963114bb17a07102030e5811db0df5900cc8
Jonathan <jonathanking.dev@gmail.com>
2023-10-25 10:47:23 +0200

    changes with solution

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+var amigos = 6;
+var cuenta = 120;
+var bebidas = 18;
+var resultado;
+
+//Invitamos a la bebida nosotros, con lo cual debemos restar al total de la cuenta los 18€ de las bebidas
+resultado = (cuenta - bebidas) / 6;
+console.log("Cada comensal debe pagar " + resultado + "€");
